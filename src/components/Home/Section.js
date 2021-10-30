d2607b0922a500e275da95407dfb2479e0f7a537
hadi21k <hadidyeb@gmail.com>
2021-10-30 21:51:51 +0300

    remove border

--- src/components/Home/Section.js
+++ src/components/Home/Section.js
@@ -47,7 +47,7 @@ const Section = ({
             <Button HomePage={true} btnText={btnText} />
           </Link>
         </div>
-        <div className="flex items-center justify-center w-full mt-8 border lg:p-5 image-section">
+        <div className="flex items-center justify-center w-full mt-8 lg:p-5 image-section">
           <img className="w-full max-w-sm md:max-w-lg animate" src={imgUrl} alt="" />
         </div>
       </div>
